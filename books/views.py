11e0dc7c42c00032305d238ebf4cde99b42601b0
EmanAmr1 <emanamr543@gmail.com>
2024-02-19 18:39:27 +0200

    add blue print to view

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app.models import Books
+from app.books import book_blueprint
 
+@book_blueprint.route('',endpoint='books_index')
 def books_index():
     books=Books.get_all_objects()
     return render_template("books/index.html",books=books)

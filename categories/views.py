bb4717246ebc38ec0e160fa87d73625407b9b162
EmanAmr1 <emanamr543@gmail.com>
2024-02-21 15:13:06 +0200

    create category model and display all categories

--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,17 @@
+from flask import render_template
 from app.categories import category_blueprint
+from app.models import Category
+
 
 @category_blueprint.route('/home' , methods=['GET'], endpoint='home')
 def category_home():
     return "<h1>welcome to category home</h2>"
+
+@category_blueprint.route('/', methods=['GET'] ,endpoint='index')
+def category_index():
+    categories= Category.get_all_categories()
+    return render_template('categories/index.html', categories=categories)
+
 
 
 
--- models.py
+++ models.py
@@ -5,7 +5,22 @@ from flask import  url_for
 
 db = SQLAlchemy()
 
+class Category(db.Model):
+    __tablename__='categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String, unique=True)
 
+    def __str__(self):
+        return f"{self.name}"
+
+    @classmethod
+    def get_all_categories(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_category_by_id(cls, id):
+        return cls.query.get_or_404(id)
 
 class Books(db.Model):
     __tablename__ = "books"
